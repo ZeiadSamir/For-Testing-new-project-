560c3645a69301dc1d472cabb1ab29a3c13adb15
unknown <zeiadsamir33@gmail.com>
2021-12-23 03:36:26 +0200

    nav

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function showNav(){
     document.getElementById('mobile-menu').addEventListener('click', function(){
-        document.getElementById('mob-main-nav').style ='display: block;';
+        document.getElementById('mob-main-nav').style ='height: 100%; opacity:1;';
         document.getElementById('mobile-menu').style = 'display: none;';
         document.getElementById('close-menu').style = 'display: block;';
     })
@@ -9,7 +9,7 @@ showNav()
 function closeNav(){
     document.getElementById('close-menu').addEventListener('click', function(){
         document.getElementById('mobile-menu').style = 'display: block;';
-        document.getElementById('mob-main-nav').style ='display: none;';
+        document.getElementById('mob-main-nav').style ='height: 0%;opacity:0;';
         document.getElementById('close-menu').style = 'display: none;';
 
     })
