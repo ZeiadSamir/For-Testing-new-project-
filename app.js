0d7e4d96de397dce90f2a27e167974a9963a7aae
unknown <zeiadsamir33@gmail.com>
2021-12-23 04:38:22 +0200

    testing

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 function showNav(){
     document.getElementById('mobile-menu').addEventListener('click', function(){
-        document.getElementById('mob-main-nav').style ='height: 100%; opacity:1;';
+        document.getElementById('mob-main-nav').style ='display: block; height: 100%; opacity:1;';
         document.getElementById('mobile-menu').style = 'display: none;';
         document.getElementById('close-menu').style = 'display: block;';
     })
 }
-showNav()
+showNav();
 function closeNav(){
     document.getElementById('close-menu').addEventListener('click', function(){
+        document.getElementById('mob-main-nav').style ='display: none; height: 0%;opacity:0;';
         document.getElementById('mobile-menu').style = 'display: block;';
-        document.getElementById('mob-main-nav').style ='height: 0%;opacity:0;';
         document.getElementById('close-menu').style = 'display: none;';
-
     })
 }
 closeNav();
+
